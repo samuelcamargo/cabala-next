ff2b8081e21c7e15f6360e2a34d212459bcca895
Samuel Camargo <samuelcamargo1@gmail.com>
2025-01-18 12:15:23 -0300

    base + mui

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { Box, Typography, Container } from '@mui/material';
+
+export default function Sobre() {
+  return (
+    <Container maxWidth="lg">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Página Sobre 2
+        </Typography>
+      </Box>
+    </Container>
+  );
+} 
